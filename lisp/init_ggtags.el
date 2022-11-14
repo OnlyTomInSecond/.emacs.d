8fb7dcf07835136d7ada342636bc9385342aedb5
OnlyTomInSecond <q2781273965@gmail.com>
2022-11-14 11:46:22 +0800

    Add ggtags support, configure ivy and extra.

---
+++ lisp/init_ggtags.el
@@ -0,0 +1,10 @@
+;; For ggtags config
+
+(require 'ggtags)
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+              (ggtags-mode 1))))
+(provide 'init_ggtags)
+
---
+++ lisp/init_ivy.el
@@ -0,0 +1,52 @@
+;;
+(use-package lsp-ivy
+  :ensure t
+  )
+(ivy-mode 1)
+(use-package counsel
+  :ensure t
+  :hook (after-init . counsel-mode)
+  :bind ( :map ivy-minibuffer-map
+          ;;Ivy-based interface to standard commands
+          ("C-s" . swiper-isearch)
+          ("M-x" . counsel-M-x)
+          ("C-x C-f" . counsel-find-file)
+          ("M-y" . counsel-yank-pop)
+          ("<f1> f" . counsel-describe-function)
+          ("<f1> v" . counsel-describe-variable)
+          ("<f1> l" . counsel-find-library)
+          ("<f2> i" . counsel-info-lookup-symbol)
+          ("<f2> u" . counsel-unicode-char)
+          ("<f2> j" . counsel-set-variable)
+          ("C-x b" . ivy-switch-buffer)
+          ("C-c v" . ivy-push-view)
+          ("C-c V" . ivy-pop-view)
+          ;;Ivy-based interface to shell and system tools
+          ("C-c c" . counsel-compile)
+          ("C-c g" . counsel-git)
+          ("C-c j" . counsel-git-grep)
+          ("C-c L" . counsel-git-log)
+          ("C-c k" . counsel-rg)
+          ("C-c m" . counsel-linux-app)
+          ("C-c n" . counsel-fzf)
+          ("C-x l" . counsel-locate)
+          ("C-c J" . counsel-file-jump)
+          ("C-S-o" . counsel-rhythmbox)
+          ("C-c w" . counsel-wmctrl)
+          ;;Ivy-resume and other commands
+          ("C-c C-r" . ivy-resume)
+          ("C-c b" . counsel-bookmark)
+          ("C-c d" . counsel-descbinds)
+          ("C-c g" . counsel-git)
+          ("C-c o" . counsel-outline)
+          ("C-c t" . counsel-load-theme)
+          ("C-c F" . counsel-org-file)
+          )
+
+  :config
+  (setq ivy-use-virtual-buffers t
+        ivy-count-format "%d/%d ")
+  )
+
+
+(provide 'init_ivy)
