fcd558450dba3cd726510b056dca015cb39cad37
OnlyTomInSecond <q2781273965@gmail.com>
2024-04-25 11:26:45 +0800

    Enable rime for emacs
    Attention!!! When using emacs-rime in arch Linux,
    emacs will crash when exits, call rime-lib-finalize before
    exiting can solve this.

--- lisp/init_text.el
+++ lisp/init_text.el
@@ -8,7 +8,7 @@
   :ensure t
   :mode ("README\\(?:\\.md\\)?\\'" . gfm-mode)
   :hook (markdown-mode . visual-line-mode)
-  
+
   :custom
   (markdown-header-scaling t)
   (markdown-enable-wiki-links t)
@@ -22,5 +22,25 @@
   :ensure nil
   :hook ((rst-mode . visual-line-mode)
          (rst-adjust . rst-toc-update)))
+
+(use-package rime
+  :ensure t
+  :defer t
+  :bind
+  (:map rime-mode-map
+        ("C-`" . 'rime-send-keybinding))
+  :config
+  ;;On arch Linux, rime will crash when emacs exits, see https://github.com/DogLooksGood/emacs-rime/issues/161
+  (setq rime-enabled 't);;Whether rime actived once or not?
+  (add-hook 'kill-emacs-hook
+            (lambda ()
+              (when rime-enabled (rime-lib-finalize)))
+            )
+  (setq rime-share-data-dir "~/.local/share/fcitx5/rime_emacs/")
+  (setq rime-user-data-dir "~/.local/share/fcitx5/rime_emacs/")
+  :custom
+  (default-input-method 'rime)
+  (rime-show-candidate 'popup)
+  )
 
 (provide 'init_text)
